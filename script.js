52f64e9b450999035672fa7158fb822097fe4ebc
David Gaynor <runnedrun@gmail.com>
2016-10-05 08:07:36 -0700

    Classadoo autocommit

---
+++ script.js
@@ -0,0 +1,16 @@
+var input = $("")
+
+input.keydown(function(e) {	
+  if (e.keyCode == "13") {
+    var valueToSave = input.val();
+    addListItem("davidsList", valueToSave);
+  }
+})
+
+clearButton.click(function(e) {	
+  deleteValue("davidsList")
+})
+
+onNewListItem("davidsList", function(value) {
+  $("#list-display").append("<div>" + value + "</div>")
+}) 
